dbd9edc91974e0e1c1118a1d0ffbaa03e3a34493
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-06-14 02:16:17 +0300

    deploy

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const connectDB = require('./config/db');
 const questionRoutes = require('./routes/questionRoutes');
 // const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const cors = require('cors');
+//! security packages
+const helmet = require("helmet");//to secure express app
+const xss = require("xss-clean");//to prevent cross site scripting
+const mongoSanitize = require("express-mongo-sanitize");//to prevent mongo injection
 dotenv.config();
 connectDB();
 
@@ -12,11 +16,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
+app.disable("x-powered-by");
+
+
+// Security
+app.use(helmet());//
+app.use(xss());
+app.use(mongoSanitize());
 
 
 app.use('/api/questions', questionRoutes);
-app.use(cors());
-app.options('*', cors());
+
 
 // app.use(notFound);
 // app.use(errorHandler);
