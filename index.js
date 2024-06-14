06ee8c3ad4ca7fdddf26e443fc6fbfcf23ce85b3
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-06-14 16:58:31 +0300

    v4

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const connectDB = require('./configdb/database');
 const questionRoutes = require('./routes/questionRoutes');
 // const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const cors = require('cors');
-//! security packages
-const helmet = require("helmet");//to secure express app
-const xss = require("xss-clean");//to prevent cross site scripting
-const mongoSanitize = require("express-mongo-sanitize");//to prevent mongo injection
+// Security packages
+const helmet = require('helmet'); // Secure express app
+const xss = require('xss-clean'); // Prevent cross site scripting
+const mongoSanitize = require('express-mongo-sanitize'); // Prevent mongo injection
+const path = require('path'); // Add path module
+
 dotenv.config();
 connectDB();
 
@@ -16,17 +18,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.options('*', cors());
-app.disable("x-powered-by");
-
+app.disable('x-powered-by');
 
 // Security
-app.use(helmet());//
+app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/questions', questionRoutes);
 
+// Serve React app for any other route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 // app.use(notFound);
 // app.use(errorHandler);
@@ -35,6 +42,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
     console.log(`App running running on port ${PORT}`);
 });
+
 process.on('unhandledRejection', (err) => {
     console.log(err.name, err.message);
     server.close(() => {
